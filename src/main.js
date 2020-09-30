9f5c6f6a9f09f99b028dc633c10f8058465d093a
andru <andru2400@hotmail.com>
2020-09-30 15:56:35 -0500

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from "vue";
 import App from "./App.vue";
+import '@/assets/css/tailwind.css';
+import '@/router';
+import router from "@/router";
+import { dollarFilter, percentFilter } from '@/filter'
+import Chart from 'chart.js'
+import Chartkick from 'vue-chartkick'
+import { VueSpinners } from '@saeris/vue-spinners'
 
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
 Vue.config.productionTip = false;
+Vue.use(VueSpinners);
+Vue.use(Chartkick.use(Chart))
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount("#app");
